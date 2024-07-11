f90013608975e7256ef72e7f3bb762e3f68ee7e8
onlyForMyKitty <egeforoffers@gmail.com>
2024-07-11 13:23:48 +0300

    3rd comm stable data view on site

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,7 +4,7 @@ namespace Drupal\home_page\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use PhpOffice\PhpSpreadsheet\IOFactory;
 class HomePageController extends ControllerBase {
   public function content() {
     return [
@@ -13,7 +13,19 @@ class HomePageController extends ControllerBase {
   }
 
   public function getData() {
-    $data = [1,2,3, "j"];
+    $path = dirname(__DIR__, 2) . '/spisok.xls';
+    $spreadsheet = IOFactory::load($path);
+    $sheet = $spreadsheet->getSheet(2);
+
+    $data = [];
+    foreach ($sheet->getRowIterator() as $row) {
+        $rowData = [];
+        foreach ($row->getCellIterator() as $cell) {
+            $rowData[] = $cell->getValue();
+        }
+        $data[] = $rowData;
+    }
+
     return new JsonResponse($data);
   }
 
